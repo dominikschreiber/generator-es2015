704afb7ce45c5e132100d939786863d5863cc25a
Dominik Schreiber <domischreib@gmail.com>
2015-06-15 11:31:16 +0200

    added simple babel + mocha + istanbul build

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,52 @@ module.exports = function(grunt) {
     require('load-grunt-tasks')(grunt);
 
     grunt.initConfig({
+        // ===== clean ==============================================
+        clean: ['./generators', './test'],
 
+        // ===== compile ============================================
+        babel: {
+            compile: {
+                expand: true,
+                src: '**/*.js',
+                dest: './generators',
+                cwd: './src/main'
+            },
+            testCompile: {
+                expand: true,
+                src: '**/*.js',
+                dest: './test',
+                cwd: './src/test'
+            }
+        },
+
+        // ===== test ===============================================
+        mochaIstanbul: {
+            coverage: {
+                src: '<%= babel.testCompile.dest %>',
+                options: {
+                    mask: '*.spec.js',
+                    check: {
+                        lines: 80,
+                        statements: 80
+                    },
+                    root: './generators',
+                    reportFormats: ['cobertura', 'lcovonly']
+                }
+            }
+        },
+
+        istanbul_check_coverage: {
+            default: {
+                options: {
+                    coverageFolder: 'coverage',
+                    check: {
+                        lines: '<%= mochaIstanbul.coverage.options.check.lines %>',
+                        statements: '<%= mochaIstanbul.coverage.options.check.statements %>'
+                    }
+                }
+            }
+        }
     });
 
     grunt.registerTask('compile-only', []);
