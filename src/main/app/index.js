7f3162cf59d62092cecf0c55f7587d7ffe2e8c60
Dominik Schreiber <domischreib@gmail.com>
2015-06-16 12:12:36 +0200

    added gruntfile creation

--- src/main/app/index.js
+++ src/main/app/index.js
@@ -1,3 +1,32 @@
 'use strict';
 
+import 'babel/polyfill';
+
 import {Base} from 'yeoman-generator';
+import {readdir} from 'fs';
+
+import {Gruntfile} from './gruntfile';
+
+export default class Generator extends Base {
+    constructor(...args) {
+        super(...args);
+    }
+
+    prompting() {
+        const done = this.async();
+
+        this.prompt([{
+            type: 'input',
+            name: 'appname',
+            message: 'your project name',
+            default: this.appname
+        }], answers => {
+            this.answers = answers;
+            done();
+        });
+    }
+
+    writing() {
+        new Gruntfile(this.gruntfile).save();
+    }
+}
